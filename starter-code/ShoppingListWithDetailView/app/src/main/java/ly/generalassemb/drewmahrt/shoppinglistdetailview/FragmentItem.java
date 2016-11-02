e8d320a9ee33730f1578057bcb0bcf2d006b5e8e
jjhiza <jjhiza@gmail.com>
2016-11-02 05:54:46 -0400

    Jay's Lab

---
+++ starter-code/ShoppingListWithDetailView/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistdetailview/FragmentItem.java
@@ -0,0 +1,41 @@
+package ly.generalassemb.drewmahrt.shoppinglistdetailview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by jay on 11/1/16.
+ */
+
+public class FragmentItem extends Fragment {
+    public static Fragment newInstance(Bundle bundle){
+        FragmentItem fragmentItem = new FragmentItem();
+        fragmentItem.setArguments(bundle);
+        return fragmentItem;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        TextView name = (TextView)view.findViewById(R.id.fTextName);
+        TextView descript = (TextView)view.findViewById(R.id.fTextDescription);
+        TextView price = (TextView)view.findViewById(R.id.fTextPrice);
+        TextView type = (TextView)view.findViewById(R.id.fTextType);
+
+        name.setText(getArguments().getString("name"));
+        descript.setText(getArguments().getString("descript"));
+        price.setText(getArguments().getString("price"));
+        type.setText(getArguments().getString("type"));
+    }
+}
--- starter-code/ShoppingListWithDetailView/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistdetailview/ShoppingListAdapter.java
+++ starter-code/ShoppingListWithDetailView/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistdetailview/ShoppingListAdapter.java
@@ -1,7 +1,5 @@
 package ly.generalassemb.drewmahrt.shoppinglistdetailview;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,10 +12,12 @@ import java.util.List;
  */
 
 public class ShoppingListAdapter extends RecyclerView.Adapter<ShoppingItemViewHolder> {
-    private List<ShoppingItem> mShoppingItems;
+    List<ShoppingItem> mShoppingItems;
+    FragmentList.OnItemSelectedListener mListener;
 
-    public ShoppingListAdapter(List<ShoppingItem> shoppingItems) {
+    public ShoppingListAdapter(List<ShoppingItem> shoppingItems, FragmentList.OnItemSelectedListener listener) {
         mShoppingItems = shoppingItems;
+        mListener = listener;
     }
 
     @Override
@@ -29,27 +29,35 @@ public class ShoppingListAdapter extends RecyclerView.Adapter<ShoppingItemViewHo
     public void onBindViewHolder(final ShoppingItemViewHolder holder, int position) {
 
         final ShoppingItem currentItem = mShoppingItems.get(position);
-
         holder.mNameTextView.setText(currentItem.getName());
-
-        // Add an OnClickListener that launches DetailActivity and passes it the item's ID
-        holder.mNameTextView.setOnClickListener(new View.OnClickListener() {
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Get a reference to the MainActivity as a Context
-                Context mainActivity = holder.mNameTextView.getContext();
-
-                // Create the intent
-                Intent intent = new Intent(mainActivity, DetailActivity.class);
-
-                // Add the ID as an extra
-                intent.putExtra(DetailActivity.ITEM_ID_KEY, currentItem.getId());
-
-                // Start the detail activity
-                mainActivity.startActivity(intent);
+                mListener.onItemSelected(mShoppingItems.get(holder.getAdapterPosition()));
             }
         });
     }
+
+//        holder.mNameTextView.setText(currentItem.getName());
+//
+//        // Add an OnClickListener that launches DetailActivity and passes it the item's ID
+//        holder.mNameTextView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+                // Get a reference to the MainActivity as a Context
+//                Context mainActivity = holder.mNameTextView.getContext();
+//
+//                // Create the intent
+//                Intent intent = new Intent(mainActivity, DetailActivity.class);
+//
+//                // Add the ID as an extra
+//                intent.putExtra(DetailActivity.ITEM_ID_KEY, currentItem.getId());
+//
+//                // Start the detail activity
+//                mainActivity.startActivity(intent);
+//            }
+//        });
+
 
     @Override
     public int getItemCount() {
